1a5afbbdf27ff2b1157a1d14927e148e88376a16
Shivang-jpg <workforshivang07@gmail.com>
2026-06-16 11:23:14 +0530

    initial commit

---
+++ src/main/java/com/url/shortener/models/ClickEvent.java
@@ -0,0 +1,20 @@
+package com.url.shortener.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import javax.naming.Name;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class ClickEvent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime clickDate;
+
+    @ManyToOne
+    @JoinColumn(name = "url_mapping_id")
+    private Url_Mapping urlMapping;
+}
