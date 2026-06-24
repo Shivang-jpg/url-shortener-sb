580d17291ed44cc492a16cdfc01fe2389a037af3
Shivang-jpg <workforshivang07@gmail.com>
2026-06-24 23:25:22 +0530

    Modified userDetailsServiceImpl and added UserRepository

--- src/main/java/com/url/shortener/service/UserDetailsServiceImpl.java
+++ src/main/java/com/url/shortener/service/UserDetailsServiceImpl.java
@@ -1,15 +1,28 @@
 package com.url.shortener.service;
 
+import com.url.shortener.models.User;
+import com.url.shortener.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-@Service 
-public class UserDetailsServiceImpl implements UserDetailsService {
+@Service
+@Component
 
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Transactional
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found with username: " + username));
+        return UserDetailsImpl.build(user);
     }
 }
