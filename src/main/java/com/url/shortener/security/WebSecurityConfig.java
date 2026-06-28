db6e943748aba04577df00a4cc361b993b325ac4
Shivang-jpg <workforshivang07@gmail.com>
2026-06-28 15:04:19 +0530

    added WebSecurityConfig in Security.jwt

---
+++ src/main/java/com/url/shortener/security/WebSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.url.shortener.security;
+
+import com.url.shortener.security.jwt.JwtAuthenticationFilter;
+import com.url.shortener.service.UserDetailsImpl;
+import com.url.shortener.service.UserDetailsServiceImpl;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@AllArgsConstructor
+public class WebSecurityConfig {
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter();
+    }
+
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("api/auth/**").permitAll()
+                        .requestMatchers("api/urls/**").authenticated()
+                        .requestMatchers("{ShortUrl}").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+
+    }
+}
