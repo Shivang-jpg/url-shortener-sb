2dcb542f93959fe96e711bd05916a0252bf6f05e
Shivang-jpg <workforshivang07@gmail.com>
2026-07-23 17:59:05 +0530

    updated the web secuirty config and dedugged and checked the login api on postman

--- src/main/java/com/url/shortener/security/WebSecurityConfig.java
+++ src/main/java/com/url/shortener/security/WebSecurityConfig.java
@@ -40,7 +40,7 @@ public class WebSecurityConfig {
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
     }
-
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
