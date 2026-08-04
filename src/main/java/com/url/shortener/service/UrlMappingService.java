8f3bb267deb2804092b4737deeeae265bfcd60ee
Shivang-jpg <workforshivang07@gmail.com>
2026-08-05 02:08:35 +0530

    resolved naming bug in UrlMappingService

--- src/main/java/com/url/shortener/service/UrlMappingService.java
+++ src/main/java/com/url/shortener/service/UrlMappingService.java
@@ -31,7 +31,7 @@ public class UrlMappingService {
 
     private UrlMappingDTO convertToDTO(UrlMapping urlMapping) {
         UrlMappingDTO urlMappingDTO = new UrlMappingDTO();
-        urlMappingDTO.setId(urlMappingDTO.getId());
+        urlMappingDTO.setId(urlMapping.getId());
         urlMappingDTO.setOriginalUrl(urlMapping.getOriginalUrl());
         urlMappingDTO.setShortUrl(urlMapping.getShortUrl());
         urlMappingDTO.setClickCount(urlMapping.getClickCount());
